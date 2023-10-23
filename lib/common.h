70b4f98a279ea8ba853bb388013376058d43df8e
Satyeshwar Singh <satyeshwar.singh@intel.com>
2023-10-23 11:20:39 -0700

    lib: Moved to a new vtotal way to sync vsyncs
    Earlier, we were synchronizing the vsyncs of two systems by modifying
    the PLL registers and this was complicated because it had to change on a
    per PHY basis. We had support for DKL and Combo PHYs but not for the
    newer C10 and C20 synopsis PHYs found on platforms like MTL and DG2.
    However, there is a much simpler mechanism to synchronize vsyncs and
    that is through the vtotal/vblank modifications. The number of registers
    that we have to modify are far fewer (only 2) and there is no
    complicated math that we must do to get the synchronization done.
    Finally, this method supports far more platforms including MTL and DG2.
    So with this change, we are moving to vtotal way of synchronizing
    vsyncs. We have removed all files for DKL and Combo phys and
    significantly changed vsyncalter.cpp to use the vtotal sync method.
    
    Signed-off-by: Satyeshwar Singh <satyeshwar.singh@intel.com>

--- lib/common.h
+++ lib/common.h
@@ -31,12 +31,20 @@ using namespace std;
 #define TRANS_DDI_FUNC_CTL_DSI1       0x6bc00
 #define DPCLKA_CFGCR0                 0x164280
 #define DPCLKA_CFGCR1                 0x1642BC
+#define TRANS_VBLANK_A                0x60010
+#define TRANS_VTOTAL_A                0x6000c
+#define TRANS_VTOTAL_B                0x6100c
+#define TRANS_VBLANK_B                0x61010
+#define TRANS_VTOTAL_C                0x6200c
+#define TRANS_VBLANK_C                0x62010
+#define TRANS_VTOTAL_D                0x6300c
+#define TRANS_VBLANK_D                0x63010
+#define TRANS_VTOTAL_EDP              0x6f00c
+#define TRANS_VBLANK_EDP              0x6f014
 #define REG(a)                        {a, 0, 0}
 
 enum {
-	DKL,
-	COMBO,
-//	C10,
+	PIPE,
 	TOTAL_PHYS,
 };
 
@@ -48,11 +56,9 @@ typedef struct _reg {
 
 class user_info {
 	private:
-	int phy_type;
 	void *phy_reg;
 	public:
-	user_info(int t, void *r) { phy_type = t; phy_reg = r; }
-	int get_type() { return phy_type; }
+	user_info(void *r) { phy_reg = r; }
 	void *get_reg() { return phy_reg; }
 };
 
@@ -60,6 +66,7 @@ typedef struct _vbl_info {
 	long *vsync_array;
 	int size;
 	int counter;
+	int pipe;
 } vbl_info;
 
 typedef int  (*find_func)();
@@ -91,12 +98,18 @@ typedef struct _platform {
 	int ds_size;
 } platform;
 
-extern platform platform_table[];
-extern int supported_platform;
-extern list<ddi_sel *> *dpll_enabled_list;
+typedef struct _trans_reg {
+	reg vblank;
+	reg vtotal;
+	int enabled;
+	int done;
+	timer_t timer_id;
+} trans_reg;
 
-int calc_steps_to_sync(double time_diff, double shift);
+int calc_time_to_sync(int vblank, double time_diff);
 void timer_handler(int sig, siginfo_t *si, void *uc);
 int make_timer(long expire_ms, void *user_ptr, timer_t *t);
+void program_regs(trans_reg *tr, int mod);
+unsigned int pipe_to_wait_for(int pipe);
 
 #endif
